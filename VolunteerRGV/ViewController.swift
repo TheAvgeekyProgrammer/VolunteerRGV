437307c9f4cf8003b9b64832a3507a3c98375462
SciAcadTSA <35090330+TheAvgeekyProgrammer@users.noreply.github.com>
2020-01-31 19:27:38 -0600

    Initialize project

---
+++ VolunteerRGV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VolunteerRGV
+//
+//  Created by Aaron Jacob on 1/28/20.
+//  Copyright © 2020 Science Academy TSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
