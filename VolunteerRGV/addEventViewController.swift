841d74535d0b9c09858904c4450da23a0dd8ca41
TheAvgeekyProgrammer <35090330+TheAvgeekyProgrammer@users.noreply.github.com>
2020-02-07 04:52:27 -0800

    Added cocoapods. Installed pods. Started on firebase. Around halfway done. Time Stamp 14:06 on Lesson 4. Still need to do the text box.

---
+++ VolunteerRGV/addEventViewController.swift
@@ -0,0 +1,36 @@
+//
+//  addEventViewController.swift
+//  VolunteerRGV
+//
+//  Created by Vivianna on 2/6/20.
+//  Copyright © 2020 Science Academy TSA. All rights reserved.
+//
+
+import UIKit
+
+class addEventViewController: UIViewController {
+
+    @IBOutlet weak var descriptionView: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addEvent(_ sender: Any) {
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
